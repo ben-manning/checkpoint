640c818dfddbe14f119cf4e4745c787db2d26ddb
Ben Manning <bmanning@me.com>
2026-03-17 10:33:36 -0400

    feat: scaffold express app structure

---
+++ server/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const healthRoutes = require('./routes/healthRoutes');
+const errorHandler = require('./middleware/errorHandler');
+require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', healthRoutes);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;

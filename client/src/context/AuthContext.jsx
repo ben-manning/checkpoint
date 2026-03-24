d262c4084b226796d89f55914868772377b1809d
Ben Manning <bmanning@me.com>
2026-03-24 15:20:25 -0400

    confirm that authcontext wrapper is applied to application

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,22 +1,29 @@
-import { useState } from 'react';
+import { createContext, useState, useContext } from 'react';
+const AuthContext = createContext();
 
-const [currentUser, setCurrentUser] = useState(null);
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
 
-const login = (userDetails) => {
-  // userDetails will be an object: { id/name/email } for the moment.
+  const login = (userDetails) => {
+    // userDetails will be an object: { id/name/email } for the moment.
 
-  if (userDetails) {
-    setCurrentUser(userDetails);
+    if (userDetails) {
+      setCurrentUser(userDetails);
+    }
   }
-}
 
-const logout = () => {
-  if (currentUser) {
-    setCurrentUser(null);
+  const logout = () => {
+    if (currentUser) {
+      setCurrentUser(null);
+    }
   }
+
+  return (
+    <AuthContext value={{ currentUser, login, logout }}>
+      { children }
+    </AuthContext>
+  )
 }
 
-export default {
-  login,
-  logout
-}
+// export const useAuth = () => useContext(AuthContext);
+export default AuthProvider;

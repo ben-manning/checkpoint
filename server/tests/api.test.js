2bfad41d70d0e3a1d0e66f28832f6215e7817afc
Ben Manning <bmanning@me.com>
2026-03-25 19:07:15 -0400

    try to remove detectOpenHandles error

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
+const pool = require('../db');
 
 describe('Health API', () => {
   it('should return 200 for /api/health', async () => {
@@ -16,4 +17,8 @@ describe('Auth API', () => {
       .send({ email: 'fake@example.com', password: 'wrong' });
     expect(res.statusCode).toBe(401);
   });
+});
+
+afterAll(async () => {
+  await pool.end();
 });
